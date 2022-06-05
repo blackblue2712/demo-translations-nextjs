52ce145ecfe99602d66ddb629556b2af2c6e3f6f
nghia dang <nghia.dang@urbn8.com>
2022-06-05 23:14:59 +0700

    feat: finish basic

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,8 +87,8 @@ const Home: NextPage = () => {
             </div>
           </div>
 
-          <button className="hidden items-center justify-center p-2 px-6 text-white bg-brightRed rounded-full hover:bg-brightRedLight md:flex">
-            Get Started
+          <button className="hidden items-center justify-center p-2 px-6 text-white bg-brightRed rounded-full capitalize hover:bg-brightRedLight md:flex">
+            {t("header:get-started")}
           </button>
 
           <button ref={ref} onClick={onHamburgerClick} id="menu-btn" className="block hamburger md:hidden focus:outline-none">
@@ -124,8 +124,8 @@ const Home: NextPage = () => {
         <div className="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:space-y-0 md:flex-row">
           {/* left items */}
           <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
-            <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
-              Birng everyone together to build better product
+            <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left" dir={dir}>
+              {t("header:first-heading")}
             </h1>
             <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
               Manage makes it simple for software teams to plan day-to-day tasks
